2a1027a87925291a089a151c8f7d5a8bd0412891
Alvin Chinedu <therealalv@gmail.com>
2022-05-28 23:24:00 +0100

    header updates

---
+++ app/js/script.js
@@ -0,0 +1,15 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+
+const header = document.querySelector('.header');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('clicked hamburger');
+
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
+    }
+    else {
+        header.classList.add('open');
+    }
+
+});
